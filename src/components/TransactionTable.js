3f7992292c45ed026a55c5fd01d53e7622323f79
judy-chelangat <judysigilai75@gmail.com>
2023-07-22 00:17:27 +0300

    switched the categories and amount to appear in order

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -7,19 +7,19 @@ return(
     <table>
     <thead>
       <tr>
-        <th>Date</th>
-        <th>Description</th>
         <th>Category</th>
+        <th>Description</th>
         <th>Amount</th>
+        <th>Date</th>
       </tr>
     </thead>
     <tbody>
       {transactionDetails.map((transaction) => (
         <tr key={transaction.id}>
-          <td>{transaction.date}</td>
-          <td>{transaction.description}</td>
-          <td>{transaction.category}</td>
-          <td>{transaction.amount}</td>
+             <td>{transaction.category}</td>
+             <td>{transaction.description}</td>
+             <td>{transaction.amount}</td>
+             <td>{transaction.date}</td>
         </tr>
       ))}
     </tbody>
