b119be54ce4ed7ef804edc5ebc089cd28977b3bb
judy-chelangat <judysigilai75@gmail.com>
2023-07-21 23:27:16 +0300

    added the form component

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        
+      </form>
+    </div>
+  )
+}
+
+export default Form;
+// Fill out and submit the form to add a new transaction.
+// This should add the new transaction to the table *the new transaction does not have to be persisted to the backend*
